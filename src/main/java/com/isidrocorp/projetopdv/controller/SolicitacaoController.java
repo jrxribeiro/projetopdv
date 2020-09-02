84ab23300f1d97c883f7a55e4d18972afbc0b5b8
professorisidro <isidro@professorisidro.com.br>
2020-09-02 15:04:16 -0300

    Atualizando solicitações

--- src/main/java/com/isidrocorp/projetopdv/controller/SolicitacaoController.java
+++ src/main/java/com/isidrocorp/projetopdv/controller/SolicitacaoController.java
@@ -3,7 +3,12 @@ package com.isidrocorp.projetopdv.controller;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.isidrocorp.projetopdv.dao.SolicitacaoDAO;
@@ -22,4 +27,49 @@ public class SolicitacaoController {
 		lista = (ArrayList<Solicitacao>)dao.findAll();
 		return lista;
 	}
+	
+	@GetMapping("/solicitacao/status/{situacao}")
+	public ArrayList<Solicitacao> recuperarPorSituacao(@PathVariable int situacao){
+		ArrayList<Solicitacao> lista;
+		lista = dao.findBySituacao(situacao);
+		return lista;
+	}
+	
+	@PostMapping("/solicitacao/nova")
+	public ResponseEntity<Solicitacao> adicionarNova(@RequestBody Solicitacao novaSolicitacao){
+		try {
+			dao.save(novaSolicitacao);
+			return ResponseEntity.status(201).body(novaSolicitacao);
+		}
+		catch(Exception ex) {
+			return ResponseEntity.badRequest().build();
+		}
+	}
+	
+	/* para atualizar uma solicitação, eu preciso preservar os dados originais, correto?
+	 * como proceder: já que eu passo apenas o ID e a SITUAÇÃO, como devo atualizar sem perder os dados
+	 * 1 - recuperar a solicitação do banco
+	 * 2 - atualizar com novo STATUS
+	 * 3 - regravar
+	 */
+	@PutMapping("/solicitacao/atualiza")
+	public ResponseEntity<Solicitacao> atualizarSolicitacao(@RequestBody Solicitacao solicitacao){
+		try {
+			Solicitacao original = dao.findById(solicitacao.getNumSeq()).orElse(null);
+			if (original != null) {
+				original.setSituacao(solicitacao.getSituacao()); // aqui eu atribuo para a solicitação original o novo valor do STATUS
+				dao.save(original);
+				return ResponseEntity.ok(original);
+			}
+			else {
+				return ResponseEntity.notFound().build();
+			}
+			
+		}
+		catch(Exception ex) {
+			return ResponseEntity.badRequest().build();
+		}
+	}
+	
+	
 }
--- src/main/java/com/isidrocorp/projetopdv/dao/SolicitacaoDAO.java
+++ src/main/java/com/isidrocorp/projetopdv/dao/SolicitacaoDAO.java
@@ -1,9 +1,13 @@
 package com.isidrocorp.projetopdv.dao;
 
+import java.util.ArrayList;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.isidrocorp.projetopdv.model.Solicitacao;
 
 public interface SolicitacaoDAO extends CrudRepository<Solicitacao, Integer>{
+	
+	public ArrayList<Solicitacao> findBySituacao(int situacao);
 
 }
