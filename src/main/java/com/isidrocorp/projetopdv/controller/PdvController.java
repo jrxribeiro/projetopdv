6a2e135881ecd079cd1ec992a193d0ec89565052
jrxribeiro <jrxribeiro@gmail.com>
2020-09-03 11:36:39 -0300

    sincronizando

--- src/main/java/com/isidrocorp/projetopdv/controller/PdvController.java
+++ src/main/java/com/isidrocorp/projetopdv/controller/PdvController.java
@@ -3,12 +3,14 @@ package com.isidrocorp.projetopdv.controller;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.isidrocorp.projetopdv.dao.PdvDAO;
 import com.isidrocorp.projetopdv.model.Pdv;
 
+@CrossOrigin("*")
 @RestController
 public class PdvController {
 	
--- src/main/java/com/isidrocorp/projetopdv/controller/SolicitacaoController.java
+++ src/main/java/com/isidrocorp/projetopdv/controller/SolicitacaoController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.isidrocorp.projetopdv.dao.SolicitacaoDAO;
 import com.isidrocorp.projetopdv.model.Solicitacao;
 
+@CrossOrigin("*")
 @RestController
 public class SolicitacaoController {
 
